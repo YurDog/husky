e874d6c359c774a205180ee3a2c83c9cd0e19cf3
七爷 <wangzz@bw30.com>
2017-11-27 20:01:34 +0800

    初始化

---
+++ husky.model/src/main/java/org/husky/model/App.java
@@ -0,0 +1,13 @@
+package org.husky.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ husky.service/src/main/java/org/husky/service/App.java
@@ -0,0 +1,13 @@
+package org.husky.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
